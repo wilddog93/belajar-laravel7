245e1091825a865589a14aa169658a70afa969a5
ridhoajibx <ridhoajibx@gmail.com>
2020-05-04 19:04:37 +0700

    eps 11 elequent class model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function show($slug)
     {
-    	$post = \DB::table('posts')->where('slug', $slug)->first();
-    	//artinya = $post = select from table posts where slug = $slug
+    	$post = Post::where('slug', $slug)->firstorFail();
+        //artinya = $post = select from table posts where slug = $slug
 
     	return view('posts.show', compact('post'));
     }
